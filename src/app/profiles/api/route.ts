810e66beef64f08f5af78bd97085fcbcbc8c4e31
Jean <89807593+naejshaw@users.noreply.github.com>
2024-11-11 22:02:05 +0000

    cache

--- src/app/profiles/api/route.ts
+++ src/app/profiles/api/route.ts
@@ -4,19 +4,21 @@ import {headers, cookies} from 'next/headers'
 export async function GET(request: NextRequest){
     const requestHeaders = new Headers(request.headers)
     const headerList = await headers()
-
-    cookies().set('token', '123abc')
-    const theme = request.cookies.get('theme')
+    const cookieStore = await cookies()
+    cookieStore.set('token', '123abc')
+    cookieStore.set('theme', 'dark')
+    // const theme = request.cookies.get('theme')
     
     console.log(requestHeaders.get('Authorization'))
     console.log(headerList.get('Authorization'))
-    console.log(theme)
-    console.log(cookies().get('token'))
+    // console.log(theme)
+    console.log(cookieStore.get('theme'))
+    console.log(cookieStore.get('token'))
 
     return new Response('<h1>Profile</h1>', {
         headers: {
             "Content-Type": "text/html",
-            "Set-Cookie": "theme=dark"
+            // "Set-Cookie": "theme=dark"
         }
     })
 }
